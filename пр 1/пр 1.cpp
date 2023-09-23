94e0ec926ab0a1e86cb5e27cd0b13e6fb4eecb63
Admin <Admin@LAPTOP-JEJVJR9H>
2023-09-23 09:32:25 +0700

    Добавьте файлы проекта.

---
+++ пр 1/пр 1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct FileData {
+	string filename;
+	string creationDate;
+	int fileSize;
+
+	FileData(const string& name, const string& date, int size)
+		: filename(name), creationDate(date), fileSize(size) {}
+};
+
+int main() {
+	setlocale(LC_ALL, "Ru");
+	vector<FileData> files;
+	ifstream inputFile("input.txt");
+	string filename;
+	string date;
+	int size;
+
+	while (getline(inputFile, filename, ',')) {
+		if (getline(inputFile, date, ',')) {
+			if (inputFile >> size) {
+				files.emplace_back(filename, date, size);
+				inputFile.ignore();
+			}
+			else {
+				cout << "Некорректный формат ввода." << endl;
+				inputFile.close();
+				return 1;
+			}
+		}
+	}
+
+	for (const FileData& file : files) {
+		cout << "Название файла: " << file.filename << endl;
+		cout << "Дата создания: " << file.creationDate << endl;
+		cout << "Размер (в байтах): " << file.fileSize << endl;
+		cout << "________________" << endl;
+	}
+
+	inputFile.close();
+}
