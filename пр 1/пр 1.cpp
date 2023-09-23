a57884f1476522383aa21998ffa6ee1006212fab
NQFV <nikita67331@gmail.com>
2023-09-23 09:47:23 +0700

    Revert "В этом улучшенном коде мы создали две функции readDataFromFile и printFileData, каждая из которых выполняет конкретную задачу, соблюдая принцип SRP. Также мы добавили обработку ошибок при открытии файла и некорректном формате ввода. Кроме того, код отформатирован в соответствии с соглашением о кодировании для улучшения его читаемости."
    This reverts commit 6232de46c05b7587ced46861dd06cc5f811d36b6.

--- пр 1/пр 1.cpp
+++ пр 1/пр 1.cpp
@@ -5,63 +5,42 @@
 using namespace std;
 
 struct FileData {
-    string filename;
-    string creationDate;
-    int fileSize;
+	string filename;
+	string creationDate;
+	int fileSize;
 
-    FileData(const string& name, const string& date, int size)
-        : filename(name), creationDate(date), fileSize(size) {}
+	FileData(const string& name, const string& date, int size)
+		: filename(name), creationDate(date), fileSize(size) {}
 };
 
-vector<FileData> readDataFromFile(const string& filename) {
-    vector<FileData> files;
-    ifstream inputFile(filename);
-
-    if (!inputFile.is_open()) {
-        cerr << "Не удалось открыть файл." << endl;
-        return files;
-    }
-
-    string line;
-    while (getline(inputFile, line)) {
-        size_t pos1 = line.find(',');
-        size_t pos2 = line.find(',', pos1 + 1);
-
-        if (pos1 != string::npos && pos2 != string::npos) {
-            string name = line.substr(0, pos1);
-            string date = line.substr(pos1 + 1, pos2 - pos1 - 1);
-            int size = stoi(line.substr(pos2 + 1));
-
-            files.emplace_back(name, date, size);
-        }
-        else {
-            cerr << "Некорректный формат ввода." << endl;
-            files.clear();
-            break;
-        }
-    }
-
-    inputFile.close();
-    return files;
-}
-
-void printFileData(const vector<FileData>& files) {
-    for (const FileData& file : files) {
-        cout << "Название файла: " << file.filename << endl;
-        cout << "Дата создания: " << file.creationDate << endl;
-        cout << "Размер (в байтах): " << file.fileSize << endl;
-        cout << "________________" << endl;
-    }
-}
-
 int main() {
-    setlocale(LC_ALL, "Ru");
-    const string inputFilename = "input.txt";
-    vector<FileData> files = readDataFromFile(inputFilename);
+	setlocale(LC_ALL, "Ru");
+	vector<FileData> files;
+	ifstream inputFile("input.txt");
+	string filename;
+	string date;
+	int size;
 
-    if (!files.empty()) {
-        printFileData(files);
-    }
+	while (getline(inputFile, filename, ',')) {
+		if (getline(inputFile, date, ',')) {
+			if (inputFile >> size) {
+				files.emplace_back(filename, date, size);
+				inputFile.ignore();
+			}
+			else {
+				cout << "Некорректный формат ввода." << endl;
+				inputFile.close();
+				return 1;
+			}
+		}
+	}
 
-    return 0;
+	for (const FileData& file : files) {
+		cout << "Название файла: " << file.filename << endl;
+		cout << "Дата создания: " << file.creationDate << endl;
+		cout << "Размер (в байтах): " << file.fileSize << endl;
+		cout << "________________" << endl;
+	}
+
+	inputFile.close();
 }
